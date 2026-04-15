1605ecfaf2db3c95232deb83b1f86e4fd404d2c1
alumno <padilla.hdz.da00@gmail.com>
2026-04-15 07:20:22 -0600

    Primer commit

---
+++ Shimeji/Shimeji/ShimejiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShimejiApp.swift
+//  Shimeji
+//
+//  Created by alumno on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct ShimejiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
